6c915a5f6ac7882f2c14c2ee38b3615a0d150674
sanskarchaurasia8 <chaurasiasanskar8@gmail.com>
2026-03-26 10:14:36 +0530

    fix socket + auth api urls

--- Frontend/src/features/chat/service/chat.socket.js
+++ Frontend/src/features/chat/service/chat.socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 
 export const initilizeSocketConnection = () => {
 
-    const socket = io("http://localhost:8000", {
+    const socket = io("https://perplexityai-3rlb.onrender.com", {
         withCredentials: true,
     })
 
