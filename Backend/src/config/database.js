854c214206b866e3c9089d8abdd7c6d27b45ce6e
sanskarchaurasia8 <chaurasiasanskar8@gmail.com>
2026-03-26 09:28:25 +0530

    fix mongo uri env variable

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/perplexity";
+    const uri = process.env.MONGO_URI || "mongodb://localhost:27017/perplexity";
     const conn = await mongoose.connect(uri);
     console.log(`MongoDB connected: ${conn.connection.host}`);
 };
